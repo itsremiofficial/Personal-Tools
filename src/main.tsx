1721cf241845e0f80e25d6f9c5e9441c679e0983
REMI <84964174+itsremiofficial@users.noreply.github.com>
2024-12-31 23:46:21 +0500

    up

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import { RouterProvider } from "react-router-dom";
-import TrayProvider from "@/components/contextProvider/TrayProvider";
 import router from "./router/routes";
+import TrayProvider from "./components/contextProvider/TrayProvider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
