d22c995669db4449b7e4733acd8dc796351415f1
REMI <84964174+itsremiofficial@users.noreply.github.com>
2026-08-12 20:03:38 +0500

    Update FileDropzone.tsx

--- src/components/FileDropzone.tsx
+++ src/components/FileDropzone.tsx
@@ -61,15 +61,16 @@ export const FileDropzone = React.memo(
     });
 
     return (
-      <Card className={cn("p-2 bg-muted/20 rounded-3xl flex flex-col gap-6 grow")}>
+      <Card
+        className={cn("p-2 bg-muted/20 rounded-3xl flex flex-col gap-6 grow")}
+      >
         <div
           {...getRootProps()}
           className={cn(
             "border-2 border-dashed p-4 sm:p-6 pb-6 sm:pb-10 rounded-3xl cursor-pointer transition-colors duration-300 group/input",
             "border-border/70 hover:border-muted-foreground/50",
             "hover:bg-muted/70",
-            isDragActive &&
-              "border-border bg-accent/70",
+            isDragActive && "border-border bg-accent/70",
             disabled && "cursor-not-allowed opacity-50",
             !disabled && "cursor-pointer",
             className,
@@ -82,8 +83,7 @@ export const FileDropzone = React.memo(
               "border-border group-hover/input:border-muted-foreground/60",
               "text-muted-foreground group-hover/input:!text-foreground",
               "transition-colors duration-300",
-              isDragActive &&
-                "!text-foreground border-muted-foreground/60",
+              isDragActive && "!text-foreground border-muted-foreground/60",
             )}
           >
             {label}
@@ -97,7 +97,9 @@ export const FileDropzone = React.memo(
             )}
           >
             <IconCloudUpload className="size-10 sm:size-14" />
-            <p className="text-xs sm:text-sm">Drag and drop files here, or click to select files</p>
+            <p className="text-xs sm:text-sm">
+              Drag and drop files here, or click to select files
+            </p>
             <p className="text-xs sm:text-sm text-muted-foreground">
               Supports SVG files only
             </p>
